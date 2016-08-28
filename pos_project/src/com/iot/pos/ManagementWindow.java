8aae0122ce0973552c687b1c357ca7e0ec12b531
jaewooHyun <faceistroll@gmail.com>
2016-08-29 08:42:15 +0900

    확인용

--- pos_project/src/com/iot/pos/ManagementWindow.java
+++ pos_project/src/com/iot/pos/ManagementWindow.java
@@ -3,6 +3,7 @@ package com.iot.pos;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -15,7 +16,7 @@ import javax.swing.JPanel;
 
 public class ManagementWindow extends JFrame implements ActionListener{
 
-	JPanel p, p2, p3, p4,p5;
+	JPanel p_center, p_east, p_north, p_west,p_south;
 	JButton bt;
 	JButton[] bt_menu;
 	JLabel[] la;
@@ -32,33 +33,43 @@ public class ManagementWindow extends JFrame implements ActionListener{
 		
 		bt = new JButton("메인화면");
 		bt_menu = new JButton[6];
-		p = new JPanel();
-		p2 = new JPanel();
-		p3 = new JPanel();
-		p4 = new JPanel();
-		p5 = new JPanel();
-		p.setPreferredSize(new Dimension(600, 450));
-		p2.setPreferredSize(new Dimension(120, 100));
-		p3.setPreferredSize(new Dimension(100, 100));
-		p4.setPreferredSize(new Dimension(100, 100));
-		p5.setPreferredSize(new Dimension(100, 100));
+		p_center = new JPanel();
+		p_east = new JPanel();
+		p_north = new JPanel();
+		p_west = new JPanel();
+		p_south = new JPanel();
+		
+		p_center.setPreferredSize(new Dimension(600, 450));
+		p_center.setLayout(new FlowLayout());
+		p_east.setPreferredSize(new Dimension(120, 100));
+		p_north.setPreferredSize(new Dimension(100, 100));
+		p_west.setPreferredSize(new Dimension(100, 100));
+		p_south.setPreferredSize(new Dimension(100, 100));
+		
 		for(int i = 0; i<bt_menu.length; i++){
-			p.add(bt_menu[i] = new JButton(bt_title[i]));
-			p.setLayout(new GridLayout(2, 3,30,30));
+			bt_menu[i] = new JButton(bt_title[i]);
+			//p.setLayout(new GridLayout(2, 3,30,30));
+			
+			p_center.add(bt_menu[i]);
+			
 			bt_menu[i].setPreferredSize(new Dimension(150, 100));
 			bt_menu[i].setFont(new Font("굴림", Font.BOLD, 15));
 			bt_menu[i].addActionListener(this);
-
+			
+			//JLabel la = new JLabel();
+			//la.add(bt_menu[i]);
+			//p_center.add();
 		}
 		
-		bt.setPreferredSize(new Dimension(100, 50));
-		p2.add(bt);
 		
-		add(p2, BorderLayout.EAST);
-		add(p, BorderLayout.CENTER);
-		add(p3, BorderLayout.NORTH);
-		add(p4, BorderLayout.WEST);
-		add(p5, BorderLayout.SOUTH);
+		bt.setPreferredSize(new Dimension(100, 50));
+		p_east.add(bt);
+		add(p_center, BorderLayout.CENTER);
+		add(p_north, BorderLayout.NORTH);
+		add(p_east, BorderLayout.EAST);
+				
+		add(p_west, BorderLayout.WEST);
+		add(p_south, BorderLayout.SOUTH);
 		
 		
 		setSize(1024, 768);
